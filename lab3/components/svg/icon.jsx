c1d449df22df6084760823f9d1b9e173877437bf
Victoriia-Ripka <vikaripka@gmail.com>
2024-10-24 15:03:24 +0200

    lab3 started

---
+++ lab3/components/svg/icon.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Icon = () => {
+    return (
+        <svg viewBox="0 0 64 64" height="1em" width="1em">
+            <path
+                d="M 0 16 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56"
+                pathLength="575.05"
+                class="path"></path>
+        </svg>
+    )
+}
+
+export default Icon;
